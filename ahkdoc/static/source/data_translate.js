bda2d350698b4741205431cf1225606ab19ced4c
hank-morelock <billy@elited.org>
2021-01-23 07:05:59 -0500

    ahk doc

---
+++ ahkdoc/static/source/data_translate.js
@@ -0,0 +1,40 @@
+translateData = {
+  // sidebar
+  "Content":true,
+  "Index":true,
+  "Filter":true,
+  "Directives":true,
+  "Built-in Variables":true,
+  "Built-in Functions":true,
+  "Control Flow Statements":true,
+  "Declarations":true,
+  "Operators":true,
+  "Commands":true,
+  "Search":true,
+  "Highlight the words":true,
+  // header
+  "Hide/Show sidebar":true,
+  "Home page":true,
+  "en":true,
+  "Change language":true,
+  "v1":true,
+  "Change AHK version":true,
+  "Edit page on GitHub":true,
+  "https://github.com/Lexikos/AutoHotkey_L-Docs/edit/master/docs/":true,
+  "Go back":true,
+  "Go forward":true,
+  "Change font size":true,
+  "Print current document":true,
+  "Open Settings":true,
+  "Change to dark/light theme":true,
+  // ALT+... shortcuts (needs uppercase)
+  "C":true,
+  "N":true,
+  "S":true,
+  // content
+  "Select code":true,
+  "Download code":true,
+  "Back to top":true,
+  "Applies to:\nAutoHotkey_L Revision {0} and later\nAutoHotkey v1.0.90.00 and later":true,
+  "Applies to AutoHotkey {0} and later":true
+};
